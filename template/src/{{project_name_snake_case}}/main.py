b8975db42aa7e1ea3f4e11f36c6ba70b3460e3bb
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-12-19 11:32:47 +0900

    feat: implement main.py with Tap

--- template/src/{{project_name_snake_case}}/main.py
+++ template/src/{{project_name_snake_case}}/main.py
@@ -1,2 +1,26 @@
+from tap import Tap
+import argcomplete
+
+class FooParser(Tap):
+    value: int = 3 # The value printed after "foo"
+
+    @staticmethod
+    def _handler(args: "FooParser") -> None:
+        print(f"foo {args.value}")
+
+    def configure(self) -> None:
+        self.set_defaults(handler=self._handler)
+
+class MainParser(Tap):
+    def configure(self) -> None:
+        self.add_subparsers(required=True)
+        self.add_subparser("foo", FooParser, help="Print foo")
+
 def main() -> None:
-    print("This is main!")
+    parser = MainParser()
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args()
+    if hasattr(args, "handler"):
+        args.handler(args)
+    else:
+        print("handler is not set")
